a5eba539ab0621b6799b884df72ef5e9a86c0532
Chau Tran <nartc7789@gmail.com>
2022-06-15 23:02:04 -0500

    init workspace

--- src/app/store-testing-lib.spec.ts
+++ src/app/store-testing-lib.spec.ts
@@ -6,15 +6,35 @@ import {SomeStore} from "./store";
 describe('store -testing lib', () => {
   let store: SomeStore;
 
-  beforeEach(async () => {
+  // working setup
+  // beforeEach(async () => {
+  //   const {debugElement} = await render(`<div></div>`, {
+  //     providers: [provideComponentStore(SomeStore)]
+  //   })
+  //
+  //   store = debugElement.injector.get(SomeStore)
+  // })
+  //
+  // it('should be fun', fakeAsync(() => {
+  //   flushMicrotasks();
+  //
+  //   // usually asserting state here after flushing microtasks;
+  //   // eg: some fetching effects are invoked in ngrxOnStateInit()
+  //
+  //   expect(store).toBeTruthy();
+  // }))
+
+  // failing setup
+  async function setupRender() {
     const {debugElement} = await render(`<div></div>`, {
       providers: [provideComponentStore(SomeStore)]
     })
 
     store = debugElement.injector.get(SomeStore)
-  })
+  }
 
-  it('should be fun', fakeAsync(() => {
+  it('should be fun', fakeAsync(async () => {
+    await setupRender();
     flushMicrotasks();
 
     // usually asserting state here after flushing microtasks;
